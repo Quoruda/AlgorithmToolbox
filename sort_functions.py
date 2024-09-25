ad12010c7274de98d37d03da03fee44d68369472
Quoruda <audrick.soltner@proton.me>
2024-09-25 13:41:57 +0200

    rename file sort.py

--- sort_functions.py
+++ sort_functions.py
@@ -19,7 +19,8 @@ def bubble_sort(arr):
                 arr[j] = temp
                 
 def insertion_sort(arr):
-    for i in range(1, len(arr)):
+    n = len(arr)
+    for i in range(1, n):
         key = arr[i]
         j = i - 1
         while j >= 0 and key < arr[j]:
@@ -27,8 +28,3 @@ def insertion_sort(arr):
             j -= 1
         arr[j + 1] = key
         
-
-
-tab  = [10, 9, 8, 7, 6, 5, 4, 3 , 2, 1]
-insertion_sort(tab)
-print(tab)
